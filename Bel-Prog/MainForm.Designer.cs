8cced2ab397a53ce35b86952a4c43bb9ff4a8cc4
tiago.ferreira <etiago@mailo.com>
2026-02-03 11:18:40 +0100

    Ajout du swap images

---
+++ Bel-Prog/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Bel_Prog
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            BtnSwap = new Button();
+            PictureRight = new PictureBox();
+            PictureLeft = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)PictureRight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PictureLeft).BeginInit();
+            SuspendLayout();
+            // 
+            // BtnSwap
+            // 
+            BtnSwap.Location = new Point(327, 121);
+            BtnSwap.Name = "BtnSwap";
+            BtnSwap.Size = new Size(146, 73);
+            BtnSwap.TabIndex = 0;
+            BtnSwap.Text = "SWAP";
+            BtnSwap.UseVisualStyleBackColor = true;
+            BtnSwap.Click += BtnSwap_Click;
+            // 
+            // PictureRight
+            // 
+            PictureRight.Image = (Image)resources.GetObject("PictureRight.Image");
+            PictureRight.Location = new Point(547, 12);
+            PictureRight.Name = "PictureRight";
+            PictureRight.Size = new Size(241, 456);
+            PictureRight.SizeMode = PictureBoxSizeMode.Zoom;
+            PictureRight.TabIndex = 1;
+            PictureRight.TabStop = false;
+            // 
+            // PictureLeft
+            // 
+            PictureLeft.Image = (Image)resources.GetObject("PictureLeft.Image");
+            PictureLeft.Location = new Point(12, 12);
+            PictureLeft.Name = "PictureLeft";
+            PictureLeft.Size = new Size(241, 456);
+            PictureLeft.SizeMode = PictureBoxSizeMode.Zoom;
+            PictureLeft.TabIndex = 2;
+            PictureLeft.TabStop = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Snow;
+            ClientSize = new Size(800, 480);
+            Controls.Add(PictureLeft);
+            Controls.Add(PictureRight);
+            Controls.Add(BtnSwap);
+            Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.IndianRed;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MainForm";
+            Text = "Bel-Prog";
+            ((System.ComponentModel.ISupportInitialize)PictureRight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PictureLeft).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnSwap;
+        private PictureBox PictureRight;
+        private PictureBox PictureLeft;
+    }
+}
---
+++ Bel-Prog/MainForm.cs
@@ -0,0 +1,41 @@
+namespace Bel_Prog
+{
+    public partial class MainForm : Form
+    {
+        private Image[] images = new Image[2];
+        private int indexLeft = 0;
+        private int indexRight = 1;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormClosing += MainForm_FormClosing;
+            images[0] = PictureLeft.Image;
+            images[1] = PictureRight.Image;
+        }
+
+        private void BtnSwap_Click(object sender, EventArgs e)
+        {
+            int tempIndex = indexLeft;
+            indexLeft = indexRight;
+            indexRight = tempIndex;
+
+            PictureLeft.Image = images[indexLeft];
+            PictureRight.Image = images[indexRight];
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Voulez-vous vraiment quitter l'application ?",
+                "Confirmation",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
